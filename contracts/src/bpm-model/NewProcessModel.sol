db8e187ee60dca6ef44bea6fa50e8753d094eba7
Jan Hendrik Scheufen <j.h.scheufen@gmail.com>
2019-02-15 13:20:24 -0500

    Added sample contracts to simulate an upgrade and added yaml upgrade scripts for service and object upgrade scenarios
    Signed-off-by: Jan Hendrik Scheufen <j.h.scheufen@gmail.com>

---
+++ contracts/src/bpm-model/NewProcessModel.sol
@@ -0,0 +1,311 @@
+pragma solidity ^0.4.25;
+
+import "commons-base/BaseErrors.sol";
+import "commons-base/ErrorsLib.sol";
+import "commons-base/AbstractVersioned.sol";
+import "commons-base/AbstractNamedElement.sol";
+import "commons-collections/Mappings.sol";
+import "commons-collections/MappingsLib.sol";
+import "commons-management/ObjectProxy.sol";
+import "commons-management/AbstractVersionedArtifact.sol";
+import "commons-management/AbstractDelegateTarget.sol";
+
+import "bpm-model/BpmModel.sol";
+import "bpm-model/DefaultProcessDefinition.sol";
+
+/**
+ * @title DefaultProcessModel
+ * @dev Default implementation of the ProcessModel interface 
+ */
+contract NewProcessModel is AbstractVersionedArtifact(1,1,0), AbstractDelegateTarget, AbstractVersioned, AbstractNamedElement, ProcessModel {
+
+	using MappingsLib for Mappings.Bytes32AddressMap;
+	using MappingsLib for Mappings.Bytes32UintMap;
+
+	Mappings.Bytes32UintMap dataDefinitions;
+	Mappings.Bytes32AddressMap processDefinitions;
+	BpmModel.ParticipantMap participants;
+	mapping(bytes32 => bool) processInterfaces;
+	bytes32[] processInterfaceKeys;
+	string modelFileReference;
+	address author;
+	bool privateFlag;
+
+	/**
+	 * @dev Initializes this DefaultProcessModel with the provided parameters. This function replaces the
+	 * contract constructor, so it can be used as the delegate target for an ObjectProxy.
+	 * @param _id the model ID
+	 * @param _name the model name
+	 * @param _version the model version
+	 * @param _author the model author
+	 * @param _isPrivate indicates if model is visible only to creator
+	 * @param _modelFileReference the reference to the external model file from which this ProcessModel originated
+	 */
+	function initialize(bytes32 _id, string _name, uint8[3] _version, address _author, bool _isPrivate, string _modelFileReference)
+		external
+		pre_post_initialize
+	{
+		id = _id;
+		name = _name;
+		semanticVersion = _version;
+		author = _author;
+		modelFileReference = _modelFileReference;
+		emit LogProcessModelCreation(
+			EVENT_ID_PROCESS_MODELS,
+			address(this),
+			_id,
+			_name,
+			_version[0],
+			_version[1],
+			_version[2],
+			_author,
+			_isPrivate,
+			false,
+			_modelFileReference
+		);
+	}
+	
+	/**
+	 * @dev Creates a new process definition with the given parameters in this ProcessModel.
+	 * REVERTS if:
+	 * - a ProcessDefinition with the same ID already exists in the ProcessModel
+	 * - the new ProcessDefinition cannot be added to the #definitions mapping
+	 * @param _processDefinitionId the process definition ID
+	 * @param _artifactsFinder an ArtifactFinder instance to create the ObjectProxy
+	 * @return newAddress - the address of the new ObjectProxy for the ProcessDefinition when successful
+	 */
+	function createProcessDefinition(bytes32 _processDefinitionId, address _artifactsFinder) external returns (address newAddress) {
+		ErrorsLib.revertIf(processDefinitions.exists(_processDefinitionId),
+			ErrorsLib.RESOURCE_ALREADY_EXISTS(), "DefaultProcessModel.createProcessDefinition", "A ProcessDefinition with the same ID already exists in this ProcessModel");
+		newAddress = new ObjectProxy(_artifactsFinder, OBJECT_CLASS_PROCESS_DEFINITION);
+		ProcessDefinition(newAddress).initialize(_processDefinitionId, address(this));
+		ErrorsLib.revertIf(processDefinitions.insert(_processDefinitionId, newAddress) != BaseErrors.NO_ERROR(),
+			ErrorsLib.INVALID_STATE(), "DefaultProcessModel.createProcessDefinition", "Unable to add the new ProcessDefinition to the collection");
+	}
+	
+	/**
+	 * @dev Returns the address of the ProcessDefinition with the specified ID
+	 * @param _id the process ID
+	 * @return the address of the process definition, if it exists
+	 */
+	function getProcessDefinition(bytes32 _id) external view returns (address) {
+		return processDefinitions.rows[_id].value;
+	}
+
+	/**
+	 * @dev Returns the file reference for the model file
+	 * @return the external file reference
+	 */
+	function getModelFileReference() external view returns (string) {
+		return modelFileReference;
+	}
+
+	/**
+	 * @dev Returns model author address
+	 * @return the model author
+	 */
+	function getAuthor() external view returns (address) {
+		return author;
+	}
+
+	/**
+	 * @dev Returns whether the model is private
+	 * @return true if the model is private, false otherwise
+	 */
+	function isPrivate() external view returns (bool) {
+		return privateFlag;
+	}
+
+	/**
+	 * @dev Adds a process interface declaration to this ProcessModel that process definitions can refer to
+	 * @param _interfaceId the ID of the interface
+	 * @return BaseErrors.RESOURCE_ALREADY_EXISTS() if an interface with the given ID already exists, BaseErrors.NO_ERROR() otherwise
+	 */
+	function addProcessInterface(bytes32 _interfaceId) external returns (uint) {
+		if (processInterfaces[_interfaceId]) return BaseErrors.RESOURCE_ALREADY_EXISTS();
+		processInterfaces[_interfaceId] = true;
+		processInterfaceKeys.push(_interfaceId);
+		return BaseErrors.NO_ERROR();
+	}
+
+	/**
+	 * @dev Adds a participant with the specified ID and attributes to this ProcessModel
+	 * @param _id the participant ID
+	 * @param _account the address of a participant account
+	 * @param _dataStorage the address of a DataStorage contract to find a conditional participant
+	 * @param _dataStorageId a field key in a known DataStorage containing an address of another DataStorage contract
+	 * @param _dataPath the field key under which to locate the conditional participant
+	 * @return BaseErrors.INVALID_PARAM_VALUE() if both participant and conditional participant are being attempted to be set or if the config for a conditional participant is missing the _dataPath
+	 * @return BaseErrors.NO_ERROR() if successful
+	 */
+	function addParticipant(bytes32 _id, address _account, bytes32 _dataPath, bytes32 _dataStorageId, address _dataStorage) external returns (uint error) {
+		if (participants.rows[_id].exists) {
+			return BaseErrors.RESOURCE_ALREADY_EXISTS();
+		}
+		// 1. single participant and conditional participant configs are mutually exclusive
+		// 2. conditional participant must have a dataPath set
+		if ((_account != 0x0 && (_dataStorage != 0x0 || _dataStorageId != "")) ||
+			(_account == 0x0 && _dataPath == "")) {
+			return BaseErrors.INVALID_PARAM_VALUE();
+		}
+
+		participants.rows[_id].keyIdx = participants.keys.push(_id);
+		participants.rows[_id].value.id = _id;
+		if (_account != 0x0) {
+			participants.rows[_id].value.account = _account;
+		} else {
+			participants.rows[_id].value.conditionalPerformer = DataStorageUtils.ConditionalData({dataStorage: _dataStorage, dataStorageId: _dataStorageId, dataPath: _dataPath, exists: true});
+		}
+		participants.rows[_id].exists = true;
+
+		return BaseErrors.NO_ERROR();
+	}
+
+	/**
+	 * @dev Returns the participant ID in this model that matches the given ConditionalData parameters.
+	 * @param _dataPath a data path
+	 * @param _dataStorageId the path to a DataStorage
+	 * @param _dataStorage the address of a DataStorage
+	 * @return the ID of a participant or an empty bytes32, if no matching participant exists
+	 */
+	function getConditionalParticipant(bytes32 _dataPath, bytes32 _dataStorageId, address _dataStorage) external view returns (bytes32) {
+		for (uint i=0; i<participants.keys.length; i++) {
+			if (participants.rows[participants.keys[i]].value.conditionalPerformer.exists &&
+				participants.rows[participants.keys[i]].value.conditionalPerformer.dataPath == _dataPath &&
+			    ((participants.rows[participants.keys[i]].value.conditionalPerformer.dataStorageId != "" &&
+			      participants.rows[participants.keys[i]].value.conditionalPerformer.dataStorageId == _dataStorageId) ||
+			     (participants.rows[participants.keys[i]].value.conditionalPerformer.dataStorage != 0x0 &&
+			      participants.rows[participants.keys[i]].value.conditionalPerformer.dataStorage == _dataStorage))) {
+					  
+					return participants.rows[participants.keys[i]].value.id;
+				}
+		}
+	}
+
+	/**
+	 * @dev Returns the number of process interfaces declared in this ProcessModel
+	 * @return the number of process interfaces
+	 */
+	function getNumberOfProcessInterfaces() external view returns (uint) {
+		return processInterfaceKeys.length;
+	}
+
+	/**
+	 * @dev Returns whether a process interface with the specified ID exists in this ProcessModel
+	 * @param _interfaceId the interface ID
+	 * @return true if it exists, false otherwise
+	 */
+	function hasProcessInterface(bytes32 _interfaceId) external view returns (bool) {
+		return processInterfaces[_interfaceId];
+	}
+
+	/**
+	 * @dev Returns the number of process definitions in this ProcessModel
+	 * @return the number of process definitions
+	 */
+	function getNumberOfProcessDefinitions() external view returns (uint) {
+		return processDefinitions.keys.length;
+	}
+
+	/**
+	 * @dev Returns the address for the ProcessDefinition at the given index
+	 * @param _idx the index position
+	 * @return the address of the ProcessDefinition, if it exists
+	 */
+	function getProcessDefinitionAtIndex(uint _idx) external view returns (address) {
+		return processDefinitions.get(processDefinitions.keys[_idx]);
+	}
+
+	/**
+	 * @dev Returns information about the ProcessDefinition at the given address
+	 * @param _processDefinition the address
+	 * @return id the process ID
+	 * @return interfaceId the first process interface the process definition supports
+	 * @return modelId the id of the model to which this process definition belongs
+	 */
+	function getProcessDefinitionData(address _processDefinition) external view returns (bytes32 id, bytes32 interfaceId, bytes32 modelId) {
+		ProcessDefinition pd = ProcessDefinition(_processDefinition);
+		id = pd.getId();
+		modelId = pd.getModelId();
+		if (pd.getNumberOfImplementedProcessInterfaces() > 0) {
+			address model;
+			(model, interfaceId) = pd.getImplementedProcessInterfaceAtIndex(0);
+		} 
+	}
+
+	/**
+	 * @dev Returns the number of participants defined in this ProcessModel
+	 * @return the number of participants
+	 */
+	function getNumberOfParticipants() external view returns (uint) {
+		return participants.keys.length;
+	}
+
+	/**
+	 * @dev Returns the ID of the participant at the given index
+	 * @param _idx the index position
+	 * @return the participant ID, if it exists
+	 */
+	function getParticipantAtIndex(uint _idx) external view returns (bytes32) {
+		return participants.keys[_idx];
+	}
+
+	/**
+	 * @dev Returns information about the participant with the given ID
+	 * @param _id the participant ID
+	 * @return location the applications contract address, only available for a service participant
+	 * @return method the function signature of the participant, only available for a service participant
+	 * @return webForm the form identifier (formHash) of the web participant, only available for a web participant
+	 */
+	function getParticipantData(bytes32 _id) external view returns (address account, bytes32 dataPath, bytes32 dataStorageId, address dataStorage) {
+		account = participants.rows[_id].value.account;
+		dataPath = participants.rows[_id].value.conditionalPerformer.dataPath;
+		dataStorageId = participants.rows[_id].value.conditionalPerformer.dataStorageId;
+		dataStorage = participants.rows[_id].value.conditionalPerformer.dataStorage;
+	}
+
+	/**
+	 * @dev Returns whether a participant with the specified ID exists in this ProcessModel
+	 * @param _id the participant ID
+	 * @return true if it exists, false otherwise
+	 */
+	function hasParticipant(bytes32 _id) external view returns (bool) {
+		return participants.rows[_id].exists;
+	}
+
+	/**
+	 * @dev Adds a data definition to this ProcessModel
+	 * The data definitions are stored under an artificial key derived as the hash of the _dataId and _dataPath parameter values.
+	 * @param _dataId the ID of the data object
+	 * @param _dataPath the path to a data value
+	 * @param _parameterType the DataTypes.ParameterType of the data object
+	 */
+	function addDataDefinition(bytes32 _dataId, bytes32 _dataPath, DataTypes.ParameterType _parameterType) external {
+		dataDefinitions.insertOrUpdate(keccak256(abi.encodePacked(_dataId, _dataPath)), uint(_parameterType));
+		emit LogProcessModelDataCreation(EVENT_ID_PROCESS_MODEL_DATA, _dataId, _dataPath, address(this), uint(_parameterType));
+	}
+
+	/**
+	 * @dev Returns the number of data definitions in the ProcessModel
+	 * @return the number of data definitions
+	 */
+	function getNumberOfDataDefinitions() external view returns (uint) {
+		return dataDefinitions.keys.length;
+	}
+
+	/**
+	 * @dev Returns details about the data definition at the given index position
+	 * REVERTS if:
+	 * - the index is out of bounds
+	 * @param _index the index position
+	 * @return key - the key of the data definition
+	 * @return parameterType - the uint representation of the DataTypes.ParameterType
+	 */
+	function getDataDefinitionDetailsAtIndex(uint _index) external view returns (bytes32 key, uint parameterType) {
+		ErrorsLib.revertIf(_index >= dataDefinitions.keys.length,
+			ErrorsLib.INVALID_INPUT(), "DefaultProcessModel.getDataDefinitionDetailsAtIndex", "The given index value is out-of-bounds of the data definitions collection");
+		key = dataDefinitions.keys[_index];
+		parameterType = dataDefinitions.get(key);
+	}
+
+}
---
+++ contracts/src/bpm-model/NewProcessModelRepository.sol
@@ -0,0 +1,249 @@
+pragma solidity ^0.4.25;
+
+import "commons-base/BaseErrors.sol";
+import "commons-utils/ArrayUtilsAPI.sol";
+import "commons-collections/Mappings.sol";
+import "commons-collections/MappingsLib.sol";
+import "commons-management/AbstractDbUpgradeable.sol";
+import "commons-management/AbstractObjectFactory.sol";
+import "commons-management/ArtifactsFinderEnabled.sol";
+import "commons-management/ObjectProxy.sol";
+
+import "bpm-model/ProcessModel.sol";
+import "bpm-model/BpmModel.sol";
+import "bpm-model/DefaultProcessModel.sol";
+import "bpm-model/ProcessModelRepository.sol";
+import "bpm-model/ProcessModelRepositoryDb.sol";
+
+/**
+ * @title DefaultProcessModelRepository
+ * @dev Default implementation of the ProcessModelRepository interface
+ */
+contract NewProcessModelRepository is AbstractVersionedArtifact(1,1,0), AbstractObjectFactory, ArtifactsFinderEnabled, AbstractDbUpgradeable, ProcessModelRepository {
+	
+	/**
+	 * @dev Modifier to only allow calls to this ProcessModelRepository from a registered ProcessModel
+	 */
+	modifier onlyRegisteredModels() {
+		if (!ProcessModelRepositoryDb(database).modelIsRegistered(msg.sender)) return;
+		_;
+	}
+	
+	/**
+	 * @dev Factory function to instantiate a ProcessModel. The model is automatically added to this repository.
+	 * @param _id the model ID
+	 * @param _name the model name
+	 * @param _version the model version
+	 * @param _author the model author
+	 * @param _isPrivate indicates if the model is private
+	 * @param _modelFileReference the reference to the external model file from which this ProcessModel originated
+	 */
+	function createProcessModel(bytes32 _id, string _name, uint8[3] _version, address _author, bool _isPrivate, string _modelFileReference) external returns (uint error, address modelAddress) {
+		modelAddress = new ObjectProxy(artifactsFinder, OBJECT_CLASS_PROCESS_MODEL);
+		ProcessModel(modelAddress).initialize(_id, _name, _version, _author, _isPrivate, _modelFileReference);
+		error = ProcessModelRepositoryDb(database).addModel(_id, _version, modelAddress);
+		ErrorsLib.revertIf(error != BaseErrors.NO_ERROR(),
+			ErrorsLib.INVALID_INPUT(), "DefaultProcessModelRepository.createProcessModel", "Unable to add the new ProcessModel to the DB contract due to invalid input");
+		// if there is no active model for this ID namespace, yet, then this one becomes the active one by default
+		if (!ProcessModelRepositoryDb(database).modelIsActive(_id)) {
+			ProcessModelRepositoryDb(database).registerActiveModel(_id, modelAddress);
+		}
+	}
+
+	/**
+	 * @dev Creates a new process definition with the given parameters in the provided ProcessModel.
+	 * @param _processModelAddress the ProcessModel in which to create the ProcessDefinition
+	 * @param _processDefinitionId the process definition ID
+	 * @return newAddress - the address of the new ProcessDefinition when successful
+	 */
+	function createProcessDefinition(address _processModelAddress, bytes32 _processDefinitionId) external returns (address newAddress) {
+		ErrorsLib.revertIf(_processModelAddress == address(0),
+			ErrorsLib.NULL_PARAMETER_NOT_ALLOWED(), "DefaultProcessModelRepository.createProcessDefinition", "The ProcessModel address must not be empty");
+		ErrorsLib.revertIf(_processDefinitionId == "",
+			ErrorsLib.NULL_PARAMETER_NOT_ALLOWED(), "DefaultProcessModelRepository.createProcessDefinition", "The process definition ID address must not be empty");
+		newAddress = ProcessModel(_processModelAddress).createProcessDefinition(_processDefinitionId, artifactsFinder);
+	}
+
+	/**
+	 * @dev Activates the given ProcessModel and deactivates any previously activated model version of the same ID
+	 * @param _model the ProcessModel to activate.
+	 * REVERTS if:
+	 * - the given ProcessModel ID and version are not registered in this ProcessModelRepository
+	 * - there is a registered model with the same ID and version, but the address differs from the given ProcessModel
+	 * - 
+	 */
+	function activateModel(ProcessModel _model) external returns (uint error) {
+		// check if there is an address registered for the model ID and version
+		address addr = ProcessModelRepositoryDb(database).getModel(_model.getId(), _model.getVersion());
+		ErrorsLib.revertIf(addr == 0x0,
+			ErrorsLib.RESOURCE_NOT_FOUND(), "DefaultProcessModelRepository.activateModel", "No registered model found that matches the model ID and version");
+		ErrorsLib.revertIf(addr != address(_model),
+			ErrorsLib.INVALID_INPUT(), "DefaultProcessModelRepository.activateModel", "Another ProcessModel address is already registered with the same model ID and version");
+		// re-use the addr field to lookup a previously activated model with the same ID
+		addr = ProcessModelRepositoryDb(database).getActiveModel(_model.getId());
+		ProcessModelRepositoryDb(database).registerActiveModel(_model.getId(), _model);
+		emit LogProcessModelActivation(_model.EVENT_ID_PROCESS_MODELS(), address(_model), true);
+		if (addr != 0x0 && addr != address(_model)) {
+			// previously activated model detected that should be updated
+			emit LogProcessModelActivation(_model.EVENT_ID_PROCESS_MODELS(), address(addr), false);
+		}
+		return BaseErrors.NO_ERROR();
+	}
+
+	/**
+	 * @dev Returns the address of the activated model with the given ID
+	 * @param _id the model ID
+	 * @return the model address, if it exists and has an active version
+	 */
+	function getModel(bytes32 _id) external view returns (address) {
+		return ProcessModelRepositoryDb(database).getActiveModel(_id);
+	}
+
+	/**
+	 * @dev Returns the address of the model with the given ID and version
+	 * @param _id the model ID
+	 * @param _version the model version
+	 * @return the model address, if found
+	 */	
+	function getModelByVersion(bytes32 _id, uint8[3] _version) external view returns (uint error, address modelAddress) {
+		modelAddress = ProcessModelRepositoryDb(database).getModel(_id, _version);
+		error = (modelAddress == 0x0) ? BaseErrors.RESOURCE_NOT_FOUND() : BaseErrors.NO_ERROR();
+	}
+	
+	/**
+	 * @dev Returns the number of models in this repository.
+	 * @return size - the number of models
+	 */
+	function getNumberOfModels() external view returns (uint size) {
+		return ProcessModelRepositoryDb(database).getNumberOfModels();
+	}
+
+	/**
+	 * @dev Returns the address of the ProcessModel at the given index position, if it exists
+	 * @param _idx the index position
+	 * @return the model address
+	 */
+	function getModelAtIndex(uint _idx) external view returns (address) {
+		return ProcessModelRepositoryDb(database).getModelAtIndex(_idx);
+	}
+
+	/**
+	 * @dev Returns information about the ProcessModel at the given address
+	 * @param _model the ProcessModel address
+	 * @return id - the model ID
+	 * @return name - the model name
+	 * @return versionMajor - the model's major version
+	 * @return versionMinor - the model's minor version
+	 * @return versionPatch - the model's patch version
+	 * @return author - the model's author
+	 * @return isPrivate - indicates if model is private
+	 * @return active - whether the model is active
+	 * @return modelFileReference - the external reference of the model file
+	 */
+	function getModelData(address _model) external view returns (bytes32 id, string name, uint versionMajor, uint versionMinor, uint versionPatch, address author, bool isPrivate, bool active, string modelFileReference) {
+		ProcessModel m = DefaultProcessModel(_model);
+		id = m.getId();
+		name = m.getName();
+		versionMajor = m.getVersionMajor();
+		versionMinor = m.getVersionMinor();
+		versionPatch = m.getVersionPatch();
+		author = m.getAuthor();
+		isPrivate = m.isPrivate();
+		active = ProcessModelRepositoryDb(database).getActiveModel(m.getId()) == _model;
+		modelFileReference = m.getModelFileReference();
+	}
+
+	/**
+	 * @dev Returns the process definition address when the model ID and process definition ID are provided
+	 * @param _modelId - the ProcessModel ID
+	 * @return _processId - the ProcessDefinition ID
+	 * @return address - the ProcessDefinition address
+	 */
+	function getProcessDefinition (bytes32 _modelId, bytes32 _processId) external view returns (address) {
+		address modelAddress = ProcessModelRepositoryDb(database).getActiveModel(_modelId);
+		if (modelAddress != 0x0) {
+			return ProcessModel(modelAddress).getProcessDefinition(_processId);
+		} else {
+			return 0x0;
+		}
+	}
+
+	/**
+	 * @dev Returns the number of process definitions in the specified model
+	 * @param _model a ProcessModel address
+	 * @return size - the number of process definitions
+	 */
+	function getNumberOfProcessDefinitions(address _model) external view returns (uint size) {
+		return ProcessModel(_model).getNumberOfProcessDefinitions();
+	}
+
+	/**
+	 * @dev Returns the address of the ProcessDefinition at the specified index position of the given model
+	 * @param _model a ProcessModel address
+	 * @param _idx the index position
+	 * @return the ProcessDefinition address, if it exists
+	 */
+	function getProcessDefinitionAtIndex(address _model, uint _idx) external view returns (address) {
+		return ProcessModel(_model).getProcessDefinitionAtIndex(_idx);
+	}
+
+	/**
+	 * @dev Returns data about the specified ProcessDefinition in the specified ProcessModel
+	 * @param _model a ProcessModel address
+	 * @param _processDefinition a ProcessDefinition address
+	 * @return id - the ProcessDefinition ID
+	 * @return interfaceId - the ProcessDefinition's first interface ID
+	 * @return modelId the id of the model to which this process definition belongs
+	 */
+	function getProcessDefinitionData(address _model, address _processDefinition) external view returns (bytes32 id, bytes32 interfaceId, bytes32 modelId) {
+		return ProcessModel(_model).getProcessDefinitionData(_processDefinition);
+	}
+
+	/**
+	 * @dev Returns the number of Activity Definitions in the specified Process Definition
+	 * The first param "address" is the model address. It's not named explicitly to avoid compiler warnings due to it not being used.
+	 * @param _processDefinition a Process Definition address
+	 * @return uint - the number of Activity Definitions
+	 */
+	function getNumberOfActivities (address, address _processDefinition) external view returns (uint size) {
+		return ProcessDefinition(_processDefinition).getNumberOfActivities();
+	}
+
+	/**
+	 * @dev Returns the ID of the ActivityDefinition at the specified index position of the given Process Definition
+	 * The first param "address" is the model address. It's not named explicitly to avoid compiler warnings due to it not being used.
+	 * @param _processDefinition a Process Definition address
+	 * @param _index the index position
+	 * @return bytes32 the ActivityDefinition ID, if it exists
+	 */
+	function getActivityAtIndex(address, address _processDefinition, uint _index) external view returns (bytes32) {
+		return ProcessDefinition(_processDefinition).getActivityAtIndex(_index);
+	}
+
+	/**
+	 * @dev Returns information about the activity definition with the given ID.
+	 * The first param "address" is the model address. It's not named explicitly to avoid compiler warnings due to it not being used.
+	 * @param _processDefinition a Process Definition address
+	 * @param _id the bytes32 id of the activity definition
+	 * @return activityType the BpmModel.ActivityType as uint8
+	 * @return taskType the BpmModel.TaskType as uint8
+	 * @return taskBehavior the BpmModel.TaskBehavior as uint8
+	 * @return assignee the ID of the activity's assignee (for interactive activities)
+	 * @return multiInstance whether the activity is a multi-instance
+	 * @return application the activity's application
+	 * @return subProcessModelId the ID of a process model (for subprocess activities)
+	 * @return subProcessDefinitionId the ID of a process definition (for subprocess activities)
+	 */
+	function getActivityData(address, address _processDefinition, bytes32 _id) external view returns (uint8 activityType, uint8 taskType, uint8 taskBehavior, bytes32 assignee, bool multiInstance, bytes32 application, bytes32 subProcessModelId, bytes32 subProcessDefinitionId) {
+		return ProcessDefinition(_processDefinition).getActivityData(_id);
+	}
+
+	function verifyOldRepositoryDisabled(address _oldRepo) external returns (bool) {
+		// this should not succeed due to the DB having changed system ownership
+		// for testing purposes only!
+		string memory someString = "bla";
+		uint8[3] memory version = [3,5,1];
+		return !_oldRepo.call(abi.encodeWithSignature("createProcessModel(bytes32,string,uint8[3],address,bool,string)",
+					bytes32("modelId"), someString, version, address(this), false, someString));
+	}
+}
