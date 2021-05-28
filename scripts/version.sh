5e3b4fd1698b156987d1f02a1110c7d167a3fbed
Silas Davis <silas@monax.io>
2021-05-28 18:47:10 +0200

    Update version script
    Signed-off-by: Silas Davis <silas@monax.io>

--- scripts/version.sh
+++ scripts/version.sh
@@ -3,6 +3,12 @@
 version=$(git describe --tags)
 version=${version#v}
 # Make semver compliant by dot-separating the <prerelease> trailer
-version="${version%-*}.${version##*-}"
+main_version="${version%-*}"
+trailer="${version##*-}"
+
+if [[ "$main_version" != "$trailer" ]]; then
+  version="$main_version.$trailer"
+fi
+
 echo "$version"
 
